62b7db43000771ba3b3712bacd72e013752ee3f0
Theresa <tli2@cca.com>
2019-05-06 16:20:00 -0700

    Implement get requests for lists

--- server/index.js
+++ server/index.js
@@ -6,8 +6,28 @@ var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
+app.get('/cars', function (req, res) {
+  items.selectAllCars(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.get('/tasks', function (req, res) {
+  items.selectAllTasks(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.get('/parts', function (req, res) {
+  items.selectAllParts(function(err, data) {
     if(err) {
       res.sendStatus(500);
     } else {
