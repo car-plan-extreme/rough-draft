39b56a2e432e3ab184a080b91ad4bc1ce811b82b
Theresa <tli2@cca.com>
2019-05-05 17:27:23 -0700

    Implement TaskList component

---
+++ react-client/src/components/TaskList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactTable from 'react-table';
+
+class TaskList extends React.PureComponent {
+  render() {
+    return (
+      <div className="task-list">
+        <ReactTable 
+          data={this.props.tasks}
+          defaultPageSize={5}
+          columns={[
+            {
+              Header: "Tasks",
+              columns: [
+                {
+                  Header: "Car",
+                  accessor: "car",
+                },
+                {
+                  Header: "Due Date",
+                  accessor: "due_date",
+                },
+                {
+                  Header: "Status",
+                  accessor: "status",
+                }
+              ]
+            }
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+export default TaskList;
