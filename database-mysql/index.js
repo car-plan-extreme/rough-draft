2083511ebc868fa6bcdb35e74678b598cbf48388
Theresa <tli2@cca.com>
2019-05-06 16:19:27 -0700

    Implement queries for lists

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,15 +1,20 @@
 var mysql = require('mysql');
+var database = require('../databaseConfig.js');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'FILL_ME_IN',
-  database : 'test'
+  password : database.password,
+  database : 'carApp'
 });
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
-    if(err) {
+connection.connect((err) => {
+  if (err) console.log('error connecting: ',err.stack)
+})
+
+var selectAllCars = function(callback) {
+  connection.query('SELECT * FROM cars', function(err, results, fields) {
+    if (err) {
       callback(err, null);
     } else {
       callback(null, results);
@@ -17,4 +22,26 @@ var selectAll = function(callback) {
   });
 };
 
-module.exports.selectAll = selectAll;
+var selectAllTasks = function(callback) {
+  connection.query('SELECT * FROM tasks', function(err, results, fields) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+var selectAllParts = function(callback) {
+  connection.query('SELECT * FROM parts', function(err, results, fields) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+module.exports.selectAllCars = selectAllCars;
+module.exports.selectAllTasks = selectAllTasks;
+module.exports.selectAllParts = selectAllParts;
