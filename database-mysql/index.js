c27924cdd972bfd8c71d05473f8afd9867e027eb
Theresa <tli2@cca.com>
2019-05-06 21:59:24 -0700

    Edit queries to search for names associated with ids

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -22,8 +22,9 @@ var selectAllCars = function(callback) {
   });
 };
 
+const taskQuery = 'SELECT t.id AS id, c.model AS car, t.task AS task, t.due_date AS due_date, t.ongoing AS ongoing, t.details AS details, s.status_code AS status FROM tasks t INNER JOIN cars c ON t.car_id = c.id INNER JOIN task_statuses s ON t.status_id = s.id';
 var selectAllTasks = function(callback) {
-  connection.query('SELECT * FROM tasks', function(err, results, fields) {
+  connection.query(taskQuery, function(err, results, fields) {
     if (err) {
       callback(err, null);
     } else {
@@ -32,8 +33,9 @@ var selectAllTasks = function(callback) {
   });
 };
 
+const partsQuery = 'SELECT p.*, s.status_code AS status FROM parts p INNER JOIN part_statuses s ON p.status_id = s.id';
 var selectAllParts = function(callback) {
-  connection.query('SELECT * FROM parts', function(err, results, fields) {
+  connection.query(partsQuery, function(err, results, fields) {
     if (err) {
       callback(err, null);
     } else {
