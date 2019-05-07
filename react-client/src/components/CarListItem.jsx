dc166d7c665aee7250c0064e3adf8147ae8d1f97
Theresa <tli2@cca.com>
2019-05-07 02:13:50 -0700

    Remove unneeded file

--- react-client/src/components/CarListItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CarListItem = (props) => (
-  <div className="car-list-item">
-    {props.name || `${props.year} ${props.make} ${props.model}`}
-  </div>
-);
-
-export default CarListItem;
